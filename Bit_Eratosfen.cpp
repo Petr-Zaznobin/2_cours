ed68764631c67beefe1d4aba4fe7c0e9b08e74f2
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2023-09-18 10:35:52 +0300

    Update Bit_Eratosfen.cpp
    все еще не работает(

--- Bit_Eratosfen.cpp
+++ Bit_Eratosfen.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 class C {
@@ -6,30 +6,35 @@ class C {
 	unsigned char arr[];
 public:
 	C(int _N) {
+        N = _N;
 		unsigned char* arr = new unsigned char[N];
-		N = _N;
 	}
 
-	template <typename T>
-	int check_ind(T t) {
+    void get_arr(){
+        for (int i = 0; i< N; i++){
+            cout << arr[i];
+        }
+    }
+
+
+
+	int check_ind(int n) {
 		int ind = n / (8 * sizeof(unsigned char));
 		return ind;
 	}
 
-	template <typename T>
-	void num_to_1(int ind, T t) {
+
+	void num_to_1(int ind, int n) {
 		arr[ind] |= 1 << (n % (8 * sizeof(unsigned char)));
 	}
 
-	template <typename T>
-	void num_to_0(int ind, T t) {
-		arr[ind] |= 1 << (n % (8 * sizeof(unsigned char)));
+	void num_to_0(int ind, int n) {
+		arr[ind] |= ~1 << (n % (8 * sizeof(unsigned char)));
 	}
 
-	template <typename T>
-	void get(int ind, T t) {
-		arr[ind] & (1 << (n % (8 * sizeof(unsigned char))));
-	}
+    void get_sq(int ind, int n){
+        cout << (arr[ind] & (1<<(n%(8*sizeof (unsigned char)))));
+    }
 
 	unsigned char operator [] (int ind) const {
 		if (arr[ind] & (1 << (ind % (8 * sizeof(unsigned char))))) return 1;
@@ -37,7 +42,8 @@ public:
 	}
 
 	~C() {
-		delete[]arr;
+        N = 0;
+		delete [] &arr;
 	}
 };
 
@@ -46,12 +52,26 @@ public:
 
 int main()
 {
-	int N = 1600;
-	C r(1600);
-	for (int i = 0; i < N; i++) {
-		r.arr
-	}
-	arr.get()
-	delete[] arr; 
+	int N = 120;
+	C arr(N);
+    for (int i = 0; i< N;i++){
+        int ind = arr.check_ind(i);
+        arr.num_to_1(ind, i);
+    }
+
+    for (int i = 2; i<N;i++){
+        int ind = arr.check_ind(i);
+        for (int j = i*i; j < N; j+=i){
+            arr.num_to_0(ind, j);
+        }
+    }
+
+    for (int i = 0;i<N;i++){
+        int ind = arr.check_ind(i);
+        cout << i << ". ";
+        arr.get_sq(ind, i);
+        cout << "\n";
+    }
 }
+
 
